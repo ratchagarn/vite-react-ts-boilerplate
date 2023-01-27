5a2a81d15e392db7c5a27b14c6bba3990644f037
Ratchagarn Naewbuntad <ratchagarn@gmail.com>
2023-01-28 05:37:47 +0700

    Fix env type

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,7 @@
 /**
  * https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
  */
-export interface ImportMetaEnv {
+interface ImportMetaEnv {
   readonly VITE_APP_SERVER_PORT: number
   readonly VITE_APP_TITLE: string
   // more env variables...
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 /// <reference types="vitest" />
+/// <reference types="./src/env" />
 import { defineConfig, loadEnv, ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
-import type { ImportMetaEnv } from './src/env'
-
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv) => {
-  const env = loadEnv(mode, process.cwd()) as unknown as ImportMetaEnv
+  const env = loadEnv(mode, process.cwd()) as ImportMetaEnv
 
   return defineConfig({
     plugins: [
